3813e693b5f2f1f27238eeb31ebb8aa27409eaf5
bajarka bajarka <zamaraevrm@mer.ci.nsu.ru>
2022-10-25 17:31:00 +0700

    xcv

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+
+def random_char(char: str):
+    seq = 1
+    rnd = random.randint(0, sum(seq.values()))
+    total = 0
+
+    for k, v in seq.items():
+        total += v
+
+        if rnd <= total:
+
+            return k
